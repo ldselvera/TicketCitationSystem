400e15f58d148da6de7151330e9bd23acf5f16e8
luis selvera <luis.selvera01@utrgv.edu>
2018-02-28 20:06:04 -0600

    Course SW

--- src/Ticket/Ticket.java
+++ src/Ticket/Ticket.java
@@ -1,6 +1,7 @@
 /*
  * Author: Luis Selvera
  * Git/Github
+ * Software Engineering 3340
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
