808facb95bb85598e4b03388db7ef12ee09ca11e
luis selvera <luis.selvera01@utrgv.edu>
2018-02-28 20:03:59 -0600

    Autho added

--- src/Ticket/Ticket.java
+++ src/Ticket/Ticket.java
@@ -1,4 +1,6 @@
 /*
+ * Author: Luis Selvera
+ * Git/Github
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
